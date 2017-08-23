d1b635c44ddc6b56794b7e83c40b688b9d6eb581
mihir20 <mihir20121997@gmail.com>
2017-08-24 03:08:50 +0530

    game over logic updated

--- Assets/scripts/PlayerLogic.cs
+++ Assets/scripts/PlayerLogic.cs
@@ -1,12 +1,14 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PlayerLogic : MonoBehaviour {
 
     public GameObject projectile;
     public bool isShooting = true;
     public float shootingTime = 0.5f;
+    float projectileCollisionOffSet = 2.0f;
 
     public float playerSpeed = 0.25f;
     float maxDistanceX = 30.0f;
@@ -39,10 +41,11 @@ public class PlayerLogic : MonoBehaviour {
 
     void spwanProjectile()
     {
-        Instantiate(projectile, new Vector3(transform.position.x,transform.position.y,0), Quaternion.identity);
+        Instantiate(projectile, new Vector3(transform.position.x,transform.position.y+projectileCollisionOffSet,0), Quaternion.identity);
     }
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        if (collision.gameObject.tag=="collision") { Debug.Log("collision"); }
+        if (collision.gameObject.tag=="collision") { SceneManager.LoadScene("spaceShooter");
+        }
     }
 }
